a296bf780203bdd8da8838d664fee8ed15409c5d
BWeinberg77 <43477452+BWeinberg77@users.noreply.github.com>
2018-09-21 19:38:45 +0200

    Scroll and Mailbox Script
    Scroll, mailbox, refabs folder, spawnpoint gameobject

---
+++ Assets/Scripts/Mailbox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mailbox : MonoBehaviour {
+    private bool mailDelivered;
+    public GameObject mainCamera; // Probably need to reference for pausing?
+    private GameObject spawnpoint;
+	// Use this for initialization
+	void Start () {
+        spawnpoint = GameObject.Find("SpawnPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "Player") {
+            //pause()
+            //showtextbox()
+            setPlayerSpawn();
+        }
+    }
+
+    public void setPlayerSpawn() {
+        spawnpoint.transform.position = this.transform.position;
+    }
+    
+}
