15e38c0aed50324a2f48be0cdf779e6a06659164
BWeinberg77 <43477452+BWeinberg77@users.noreply.github.com>
2018-09-21 20:46:55 +0200

    Working mailbox collison

---
+++ Assets/Scripts/Mailbox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mailbox : MonoBehaviour {
+
+    private GameObject spawnpoint;
+    private Component dialogueTrigger;
+    public bool firstCollision = true;
+	// Use this for initialization
+	void Start () {
+        spawnpoint = GameObject.Find("SpawnPoint");
+        if (GetComponent("DialogueTrigger") != null) {
+            dialogueTrigger = GetComponent("DialogueTrigger");
+        } else {
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "Player" && firstCollision) {
+            //pause()
+            dialogueTrigger.SendMessage("TriggerDialogue");
+            movePlayerSpawnPoint();
+
+            firstCollision = false;
+        }
+    }
+
+    void movePlayerSpawnPoint() {
+        spawnpoint.transform.position = this.transform.position;
+    }
+}
