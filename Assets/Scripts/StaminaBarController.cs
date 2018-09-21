9dae768afcc9ad1c7e1f751341bac4daa6f56118
kamipenguin <darkroxy@live.nl>
2018-09-21 20:04:34 +0200

    Added stamina bar

---
+++ Assets/Scripts/StaminaBarController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBarController : MonoBehaviour
+{
+    private Image _staminaBarImage;
+
+    void Start()
+    {
+        _staminaBarImage = GetComponent<Image>();
+    }
+
+    public void OnStaminaChanged(float currentStamina)
+    {
+        _staminaBarImage.fillAmount = currentStamina;
+    }
+}
